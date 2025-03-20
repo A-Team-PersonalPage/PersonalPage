56fbc6ba482484ace14afb7986249141fdb7dcef
jieun <jieunh0311@gmail.com>
2025-03-20 17:09:24 +0900

    백엔드 태그 조회 수정

--- routes/place/placeTags.js
+++ routes/place/placeTags.js
@@ -1,46 +1,57 @@
 // routes/postTags.js
-const express = require('express');
-const db = require('../../db');
+const express = require("express");
+const db = require("../../db");
 const router = express.Router();
 
 // 특정 소개글의 태그 조회
-router.get('/', (req, res) => {
+router.get("/:place_id", (req, res) => {
   const { place_id } = req.params;
-  db.query('SELECT t.tag_name FROM place_tags pt JOIN tags t ON pt.tags_id = t.id WHERE pt.place_id = ?', [place_id], (err, results) => {
-    if (err) {
-      return res.status(500).send('서버 오류');
+  db.query(
+    "SELECT t.tag_name FROM place_tags pt JOIN tags t ON pt.tag_id = t.id WHERE pt.place_id = ?",
+    [place_id],
+    (err, results) => {
+      if (err) {
+        return res.status(500).send("서버 오류");
+      }
+      res.json(results);
     }
-    res.json(results);
-  });
+  );
 });
 
 // 특정 소개글에 태그 추가
-router.post('/:place_id', (req, res) => {
+router.post("/:place_id", (req, res) => {
   const { place_id } = req.params;
   const { tag_id } = req.body;
-  
-  console.log(place_id)
 
-  db.query('INSERT INTO place_tags (place_id, tag_id) VALUES (?, ?)', [place_id, tag_id], (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send('서버 오류');
+  console.log(place_id);
+
+  db.query(
+    "INSERT INTO place_tags (place_id, tag_id) VALUES (?, ?)",
+    [place_id, tag_id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send("서버 오류");
+      }
+      res.status(201).send("태그가 추가되었습니다.");
     }
-    res.status(201).send('태그가 추가되었습니다.');
-  });
+  );
 });
 
 // 특정 소개글에서 태그 삭제
-router.delete('/:place_id/:tag_id', (req, res) => {
+router.delete("/:place_id/:tag_id", (req, res) => {
   const { place_id, tag_id } = req.params;
 
-  db.query('DELETE FROM place_tags WHERE place_id = ? AND tag_id = ?', [place_id, tag_id], (err, result) => {
-    if (err) {
-      return res.status(500).send('서버 오류');
+  db.query(
+    "DELETE FROM place_tags WHERE place_id = ? AND tag_id = ?",
+    [place_id, tag_id],
+    (err, result) => {
+      if (err) {
+        return res.status(500).send("서버 오류");
+      }
+      res.send("태그가 삭제되었습니다.");
     }
-    res.send('태그가 삭제되었습니다.');
-  });
+  );
 });
 
 module.exports = router;
-
